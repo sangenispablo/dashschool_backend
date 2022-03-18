62e8bf18c7c55e902d698f2a51f2a9da7823d242
Sangenis Pablo <sangenispablo@gmail.com>
2022-03-18 20:19:20 -0300

    Primer Commit

--- src/database.js
+++ src/database.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose
-  .connect(
-    "mongodb://localhost:27017/schoolDB?readPreference=primary&directConnection=true"
-  )
+  .connect("mongodb://localhost:27017/schoolDB")
   .then((db) => console.log("DB Connected"))
   .catch((error) => console.log(error));
