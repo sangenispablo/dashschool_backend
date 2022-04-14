4a9dc8303a48c475b3bc8f3d5ff7c4e5d7ba71dc
Sangenis Pablo <sangenispablo@gmail.com>
2022-04-14 09:07:12 -0300

    fix config and env

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/schoolDB',
+  mongoUri: process.env.MONGODB_URI || "mongodb://localhost:27017/schoolDB",
   serverPort: process.env.SERVER_PORT || 4000,
-  SECRET: "rollingc0de",
+  SECRET: process.env.PALABRA || "rollingc0de",
   EXPIRE: "24h",
   corsOptions: {},
 };
