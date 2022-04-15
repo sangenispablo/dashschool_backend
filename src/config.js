6989ab6386a21ac76e51b7ddd2c7a66698ae4f0e
Sangenis Pablo <sangenispablo@gmail.com>
2022-04-15 01:06:31 -0300

    fix scripts

--- src/config.js
+++ src/config.js
@@ -5,7 +5,6 @@ config();
 
 export default {
   mongoUri: process.env.MONGODB_URI || "mongodb://localhost:27017/schoolDB",
-  serverPort: process.env.SERVER_PORT || 4000,
   SECRET: process.env.PALABRA || "rollingc0de",
   EXPIRE: "24h",
   corsOptions: {},
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import app from "./app";
 import dbConnect from "./database";
+import { config } from "dotenv";
+
+// Ejecuto config para que pueda meterme en el .env
+config();
 
 // Me conecto a la base de datos
 dbConnect();
 
+const PORT = process.env.SERVER_PORT || 4000;
+
 // Arranco el servidor
-app.listen(app.get("port"), () => {
-  console.log(`Servidor escuchando en el puerto ${app.get("port")}`);
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
